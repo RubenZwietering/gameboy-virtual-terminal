1fae05fdde64977dade79d3ef3972ce0aa8e095b
Ruben Zwietering <ruben.zwietering@gmail.com>
2024-07-24 14:56:31 +0200

    Add some printf tests to main.c

--- src/main.c
+++ src/main.c
@@ -5,9 +5,11 @@ extern volatile unsigned char hardware;
 extern void gotoxy(unsigned char, unsigned char);
 extern char putchar(char);
 extern char puts(const char*);
+extern int printf(const char* fmt, ...);
 
-#pragma disable_warning 115
+#pragma disable_warning 115 // sdcc ignore "unknown or unsupported #pragma directive"
 #pragma clang diagnostic ignored "-Wmain-return-type"
+#pragma clang diagnostic ignored "-Wc23-extensions"
 
 void main(void)
 {
@@ -36,4 +38,19 @@ void main(void)
 			puts("Line 8: Unknown system");
 			break;
 	}
+
+	puts("Line 9:"
+	#ifdef __SDCC_z80 
+	" __SDCC_z80"
+	#endif
+	#ifdef __SDCC_sm83
+	" __SDCC_sm83"
+	#endif
+	);
+
+	printf("Line 10: printf\n");
+	printf("%s\n", "Line 11: printf %s");
+	printf("L%cne %d: %s\nLine 13: %x==%u\n", 'i', 12, "printf %d", 0xff, 255);
+	printf("Line 14: %x==%u\n", 0xfff, 0xfff);
+	printf("Line 15: %x==%d\n", -15, -15);
 }
